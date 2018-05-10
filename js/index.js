e740d0c3a22249622a153a81aef6235e10804d83
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-05-10 23:07:31 +0100

    implemented js functionality for signin modal

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const overlay = document.getElementById('overlay');
 const sign_up_form = document.getElementById('signup-form');
+const sign_in_form = document.getElementById('signin-form');
 const sign_up_btn = document.getElementById('signup-btn')
 const sign_in_btn = document.getElementById('signin-btn')
 const request_btn = document.getElementById('request-btn')
@@ -16,7 +17,14 @@ request_btn.addEventListener('click', function (e) {
   sign_up_form.style.visibility = 'visible';
 })
 
+sign_in_btn.addEventListener('click', function (e) {
+  e.preventDefault();
+  overlay.style.visibility = 'visible';
+  sign_in_form.style.visibility = 'visible';
+})
+
 overlay.addEventListener('click', function () {
   overlay.style.visibility = 'hidden';
   sign_up_form.style.visibility = 'hidden';
+  sign_in_form.style.visibility = 'hidden';
 })
