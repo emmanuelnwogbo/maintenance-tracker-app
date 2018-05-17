c2ba757265a0a3a80943dcebf597d7f7eadaca20
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-05-17 03:36:06 +0100

    added user flag to user routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,15 +4,15 @@ const router = express.Router()
 const userRoute = router
 
 
-router.post('/signup', (req, res) => {
+router.post('/user/signup', (req, res) => {
   res.send('hello user signup');
 })
 
-router.post('/signin', (req, res) => {
+router.post('/user/signin', (req, res) => {
   res.send('hello user signin');
 })
 
-router.post('/logout', (req, res) => {
+router.post('/user/logout', (req, res) => {
   res.send('hello user logout');
 })
 
