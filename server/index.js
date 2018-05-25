2c127a1328039beb1adf68a5a2ebbc83a0cffef6
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-05-25 13:43:10 +0100

    [Feature develop]Updated file names to fit style guide

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }))
 
+app.use('/', indexRoute)
 app.use('/api/v1', indexRoute)
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/users/requests', userRequestRoute)
